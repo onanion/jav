086a23b3e6a816545a7ec38bfc91f324974f0b1d
onanion <56130444+onanion@users.noreply.github.com>
2020-07-01 19:06:07 +0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,3 +9,7 @@ $(window).scroll(function() {
     $('.top').removeClass("up");
   }
 });
+
+
+// Download Box V4
+$(document).ready(function() {$("#box-download .body-box_download p a").attr({rel:"nofollow",target:"_blank"})})
